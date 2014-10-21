18faddecc31172621d200ae1b799cd2b033a4e27
Wewe12 <ew.bukowska@gmail.com>
2014-10-21 13:37:57 +0200

    Lab 1

--- 1-threads-basics/students/src/exercise4/Main.java
+++ 1-threads-basics/students/src/exercise4/Main.java
@@ -8,5 +8,10 @@ public class Main {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(4);
 
+        for (int i = 0; i < 4; i++){
+            executor.execute(new MyRunnable());
+        }
+        executor.shutdown();
+
     }
 }
